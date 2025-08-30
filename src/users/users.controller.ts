e29024cc5d9e1375696d3f66717d5dcf020fd8b7
nizarIng <nizar.ilahi@gmail.com>
2025-08-30 23:05:40 +0100

    Using NestJS decorators to grab parameters, query key value pairs and request Body as well.

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+
+  @Get('/:id')
+  public getUsers(@Param() params: any, @Query() query: any): string {
+    console.log(params, query);
+    return "You sent a GET request to '/users' endpoint.";
+  }
+
+  @Post()
+  public createUsers(@Body() bodyRequest: any): string{
+    console.log(bodyRequest);
+    return "You sent a POST request to '/users' endpoint.";
+  }
+}
