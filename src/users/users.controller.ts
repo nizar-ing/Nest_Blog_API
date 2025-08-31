0646462b28959795b066cfb61c8eef8a4d451f2c
nizarIng <nizar.ilahi@gmail.com>
2025-08-31 16:34:22 +0100

    Performing the getUsers endpoint by using the ParseIntPipe to ensure transformation and validation of our inputs data.

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,17 +1,22 @@
-import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
+import { Body, Controller, DefaultValuePipe, Get, Param, ParseIntPipe, Post, Query } from '@nestjs/common';
 
 @Controller('users')
 export class UsersController {
 
-  @Get('/:id')
-  public getUsers(@Param() params: any, @Query() query: any): string {
-    console.log(params, query);
-    return "You sent a GET request to '/users' endpoint.";
+  @Get('/:id?')
+  public getUsers(
+    @Param('id', ParseIntPipe) id: number | undefined,
+    @Query('limit', new DefaultValuePipe(10), ParseIntPipe) limit: number,
+    @Query('page', new DefaultValuePipe(1), ParseIntPipe) page: number,
+  ): string {
+    console.log(limit);
+    console.log(page);
+    return 'You sent a GET request to \'/users\' endpoint.';
   }
 
   @Post()
-  public createUsers(@Body() bodyRequest: any): string{
+  public createUsers(@Body() bodyRequest: any): string {
     console.log(bodyRequest);
-    return "You sent a POST request to '/users' endpoint.";
+    return 'You sent a POST request to \'/users\' endpoint.';
   }
 }
