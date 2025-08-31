54d7cd9b525c73adacad33557869b539ae32e827
nizarIng <nizar.ilahi@gmail.com>
2025-08-31 17:56:56 +0100

    Creating our CreateUserDto to map properly the request body incorporated within the POST users request.

---
+++ src/users/dtos/create-user.dto.ts
@@ -0,0 +1,31 @@
+import { IsEmail, IsNotEmpty, IsOptional, IsString, Matches, MaxLength, MinLength } from 'class-validator';
+
+export class CreateUserDto {
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(3)
+  @MaxLength(96)
+  firstName: string;
+
+  @IsString()
+  @IsOptional()
+  @MinLength(3)
+  @MaxLength(96)
+  lastName?: string;
+
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(8, { message: 'Password must be at least 8 characters long' })
+  @Matches(
+    /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]/,
+    {
+      message: 'Password must contain at least one letter, one number, and one special character (@$!%*?&)',
+    }
+  )
+  password: string;
+}
